aa7eb9bf5640d4b93e8d4555faaac75c87d047d8
Hugo <hugo@Hugos-Mac.local>
2015-02-18 15:20:23 -0800

    Initial Commit

---
+++ HGInstagram/ViewController.h
@@ -0,0 +1,15 @@
+//
+//  ViewController.h
+//  HGInstagram
+//
+//  Created by Hugo on 2/18/15.
+//  Copyright (c) 2015 Hugo. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface ViewController : UIViewController
+
+
+@end
+
